46c71a8d9f342a3d7bbc5bc78139a32962c881e4
Michael JasonSmith <mpj17@onlinegroups.net>
2016-08-19 13:20:21 +0100

    Moving to a function

--- parabolamov.py
+++ parabolamov.py
@@ -4,22 +4,26 @@ from moviepy.editor import ImageClip
 
 
 # Generate the points on a parabola
-points = [(x, x**2) for x in range(-10, 10)]
+parabolaPoints = [(x, x**2) for x in range(-10, 10)]
 
-# Plot the points
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-ax.scatter([p[0] for p in points], [p[1] for p in points])
-fig.canvas.draw()
+def points_to_image(points, duration=1):
+    """Plot a list of (x, y) points to an ImageClip of given duration"""
+    # Plot the points
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter([p[0] for p in points], [p[1] for p in points])
+    fig.canvas.draw()
+    
+    # Convert the plot to 24-bit RGB data (0-255, 0-255, 0-255)
+    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
 
-# Convert the plot to 24-bit RGB data (0-255, 0-255, 0-255)
-data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
-data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    # Add the plot to a movie-clip "image"
+    retval = ImageClip(data)
+    retval.duration = duration
+    retval.fps = 60
+    return retval
 
-# Add the plot to a movie-clip "image"
-i = ImageClip(data)
-i.duration = 5
-i.fps = 60
-
+i = points_to_image(parabolaPoints)
 # Write the clip to a file
 i.write_gif('foo.gif')
