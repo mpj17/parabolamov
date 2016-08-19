4a949c02bb2242d056401941e9a3be8b024efdb0
Michael JasonSmith <mpj17@onlinegroups.net>
2016-08-19 14:50:25 +0100

    PEP-8 fixes

--- parabolamov.py
+++ parabolamov.py
@@ -5,6 +5,7 @@ from moviepy.editor import (ImageClip, concatenate_videoclips)
 
 SIZE = 100  # The size of the x-axis of the parabola plot: [-SIZE, SIZE]
 
+
 def plot(points):
     """Plot a list of (x, y) points to a matplotlib.pyplot.figure"""
     # The actual dimensions of the movie are determined by the
@@ -21,6 +22,7 @@ def plot(points):
     retval.canvas.draw()  # Render it, so we can get the RGB data later
     return retval
 
+
 def figures():
     '''Generate the parabola figures'''
     # Lots of ``+1`` here because of how Python treats ranges.
@@ -29,6 +31,7 @@ def figures():
         figure = plot(parabolaPoints[:i+1])
         yield figure
 
+
 def fig_to_rgb(fig):
     """Convert the plot to a numpy array of 24-bit RGB data"""
     # Thanks to Joe Kington on Stack Overflow
@@ -36,6 +39,7 @@ def fig_to_rgb(fig):
     data = np_fromstring(fig.canvas.tostring_rgb(), dtype=uint8, sep='')
     retval = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return retval
+
 
 def fig_to_clip(fig, duration=1):
     """Render a matplotlib.pyplot.figure to a
